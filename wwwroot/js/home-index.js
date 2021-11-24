ff61246ff142a3011ecd3e117490a1a290757c51
GregoryPrograms <greg.dost@yahoo.com>
2021-11-24 14:38:17 -0800

    added basic looping code to change what the front page displays

---
+++ wwwroot/js/home-index.js
@@ -0,0 +1,15 @@
+/* Code to loop between main page divs. First, we select all divs that contain
+    'rotate' in their id, then we loop between setting their display value
+     to ''none' and 'block'*/
+rotateDivs = document.querySelectorAll('[id ^= "rotate"]');
+rotateDivs.forEach(element => {
+    element.style.display = "none";
+});
+var divIndex = 0;
+setInterval(cycle, 10000);
+rotateDivs[divIndex].style.display = "block";
+function cycle(){
+    rotateDivs[divIndex].style.display = "none";
+    divIndex = (divIndex == rotateDivs.length - 1)? 0: divIndex + 1;
+    rotateDivs[divIndex].style.display = "block";
+}
