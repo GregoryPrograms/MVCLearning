ef29df5d2251ca0222be7bfe02353376baea01c0
GregoryPrograms <greg.dost@yahoo.com>
2021-12-02 19:43:30 -0800

    Added some TODO about what I plan to do next.

---
+++ Models/ProjectModel.cs
@@ -0,0 +1,9 @@
+//TODO: Make it so a list of projects is pulled from github in an efficient, low resource manner. Possible that this would be fine to call whenever the site is refreshed - since the # of projects would be low, overhead should be reasonable. 
+// Looks like the best way to do this would be using sessionStorage from html5.
+public class projects
+{  
+  public projects()
+    {
+        //Will add code here to pull projects from github. Will add a way to search through github for projects flagged as viable projects, then construct the class object from information about each project - which will then be used to translate to CSS.
+    }
+}
