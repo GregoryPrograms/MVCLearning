069e595f0f37c075205362dc74fba2ce67b75b51
GregoryPrograms <greg.dost@yahoo.com>
2021-12-28 10:11:09 -0800

    Added a callback function to parse list of projects - eventually, will create html objects with parsed data

--- wwwroot/js/dataSetGen.js
+++ wwwroot/js/dataSetGen.js
@@ -1,17 +1,44 @@
 //Code to generate data sets via APIs used by the website.
+class project{
+    constructor(projString){
+    this.name = projString[0];
+    this.desc = projString[1];
+    this.url = projString[2];
+    }
+}
 
-
-function projectSetConstruct(){
-     var projectsPF = [];
+function projectSetConstruct(callback){
+    var projectsPF = [];
     $.getJSON("https://api.github.com/users/GregoryPrograms/repos", function(data){
-        for(var projectIndex = 0; projectIndex < data.length; projectIndex++){
-            if(data[projectIndex].name.includes("P")){
+        for(var projectIndex = 0; projectIndex < data.length; projectIndex++){          
+            if(data[projectIndex].name.includes("SC2")){
                 projectsPF.push(data[projectIndex].name);
+                projectsPF.push(data[projectIndex].description);
+                projectsPF.push(data[projectIndex].html_url);
             }
         }
         sessionStorage.setItem('projects', JSON.stringify(projectsPF));
+        callback(sessionStorage.getItem('projects'));
     });
 }
-if(sessionStorage.getItem('projects')){
-    projectSetConstruct();
+
+function projHTMLStruct(projString){
+    projString = JSON.parse(projString);
+    projList = [];
+    projDescriptors = [];
+    for(projectIndex = 0; projectIndex < projString.length; projectIndex++){
+        projDescriptors.push(projString[projectIndex]);
+        if(projectIndex % 3 == 2){
+            projList.push(new project(projDescriptors));
+            projDescriptors = [];
+        }
+    }
+    console.log(projList);
+}
+
+
+
+if(!sessionStorage.getItem('projFlag')){
+    projectSetConstruct(projHTMLStruct);
+    sessionStorage.setItem('projFlag', 'true');
 }
